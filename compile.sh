49b1f32ae5374e94fc3be4276d7f7bf66692fd86
Caglar Gulcehre <caglar@ulakbim.gov.tr>
2010-10-10 01:46:32 +0300

    added -pedantic to compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -std=c99 -Wall -O3 -c autoexpandvector.c autoexpandvector.h 
+gcc -std=c99 -pedantic -Wall -O3 -c autoexpandvector.c autoexpandvector.h 
 gcc -std=c99 -Wall autoexpandvector.o test.c -o test
-rm ./autoexpandvec.o
+rm *.o
